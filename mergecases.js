5191424d125cbc4a9d552319a1787d9d898ae3c4
JA Vieitez <joseantonio.vieitez@gmail.com>
2019-10-21 15:26:33 +0200

    Serious refactoring, part 2

--- mergecases.js
+++ mergecases.js
@@ -39,13 +39,24 @@ function egLogout() {
 			fetch(logoutUrl, initObject);
 				}
 
+function switchMethodAndBody(x, y){
+	initObject.method = x
+	initObject.body = y
+}
+
+function validActivityStatus(z){
+if (z == 'awaiting_assignment' || z == 'assigned') {
+  return true;
+} else {
+  return false;
+}};
+
 /* fetch the case data and put it on a table*/
 function buildTableAndLogout(y) {
-	initObject.method = 'GET'
-	initObject.body = undefined
+	switchMethodAndBody('GET', null)
 	fetch(getCaseUrl, initObject)
 		.then(function(response){
-				return response.json()})
+				return response.json();})
 		.then(function(data){
 	  	window.bufferCase = data.case[0];
 			// prevent unassigned case from crashing the function
@@ -62,10 +73,12 @@ function buildTableAndLogout(y) {
 					'</TD><TD>' + window.bufferCase.subject +
 					'</TD><TD>' + window.bufferCase.dueDate + '</TD></TR></TABLE>';
 			//put the table in place
-			writeDIV(y, myTable);
-			//move the buffered object from the previous step to a fixed place
-			window.srcCaseProperties = window.bufferCase;		})
-			.then(() => {egLogout()})
+			writeDIV(y, myTable)
+			//store the source case properties only if first run
+			if (y == 'firstTable'){
+				storeFirstCase()}
+			;})
+			.then(() => egLogout())
 			.catch(function(err){
 			 	console.log("Something went wrong!", err);
 				egLogout()
@@ -90,10 +103,136 @@ function processDestCaseID() {
 	}
 }
 
+
+
+/*function for the mergeCases button*/
 function mergeCases(){
-	console.log('cases will be merged');
+	console.clear()
+	if (window.srcCaseProperties.id === window.bufferCase.id) {
+		writeDIV('actionsLog', 'both cases are the same: ' + window.bufferCase.id);
+		writeDIV('mergeCaseButton', '')
+
+	} else {
+		thirdTrigger = egLogin()
+		thirdTrigger
+		.then(() => getSourceActivityIDs()
+			.catch(() => console.log('No shit'))
+		)
+		//movea ctivities must go first, in order to reopen the case if it was closed
+
+		//then change the customer
+		.then(() => changeCaseCustomer())
+		.then(() => moveActivitiesAndLogout())
+		//.then(() => egLogout())
+
+	}}
+
+function storeFirstCase(){
+	window.srcCaseProperties = window.bufferCase;
+	}
+
+/* get the valid activity IDs of source case*/
+function getSourceActivityIDs () {
+	return new Promise(function(resolve, reject) {
+	let listActivitiesURL = baseUrl + window.srcCaseProperties.activities.link.href;
+	switchMethodAndBody('GET', null)
+	fetch(listActivitiesURL, initObject)
+	.then(function(response){
+		return response.json();})
+	.then(function(data){
+		//restart the array, then populate it with the activities
+		window.srcCaseActivityIDs =[]
+		for (i =0; i < Object.keys(data.activity).length; i++){
+			//but ONLY with activities with a valid status
+			if (validActivityStatus(data.activity[i].status.value) == true) {
+				// for those that qualify:
+				append2DIV('actionsLog',data.activity[i].id + ' is in a valid status');
+				window.srcCaseActivityIDs.push(data.activity[i].id);
+				//if customer differs, change it
+				if (data.activity[i].customer.id != window.srcCaseProperties.customer.id) {
+          fetchAndChangeActivityCustomer(data.activity[i].id, window.srcCaseProperties.customer.id)
+				}} else {
+				append2DIV('actionsLog', data.activity[i].id + ' is ' + data.activity[i].status.value);
+			}}
+			//exit if no valid activities left
+			if (window.srcCaseActivityIDs.toString() == '') {
+				append2DIV('actionsLog', 'Nothing can be moved from the source to the destination case');
+				reject('no valid activity IDs');
+
+			} else {
+				append2DIV('actionsLog', window.srcCaseActivityIDs.toString() + ' will be moved');
+				resolve('activity IDs changed: ' + window.srcCaseActivityIDs.toString())
+			}
+
+			})})
+//			return promise;
 }
 
-//Ready, steady, GO!!!
-egLogin()
+function fetchAndChangeActivityCustomer(activity, customer) {
+//must fetch the contact point ID first
+var tempURL = baseUrl + window.bufferCase.customer.link.href
+switchMethodAndBody('GET', null)
+fetch(tempURL, initObject)
+	.then(function(response){
+		return response.json();})
+	.then(function(data){
+		contactPointID = data.customer[0].contactPersons.contactPerson[0].contactPoints.contactPoint[0].id
+		return(window.contactPointID);})
+	.then(function(w){
+		changeActivityCustomer(activity, w)
+	})}
+
+// Move the activities to the destination case
+function moveActivitiesAndLogout() {
+	return new Promise(function(resolve, reject) {
+		let moveactivityURL = baseUrl + '/system/ws/v12/interaction/activity/' + window.srcCaseActivityIDs.toString().replace(/[,]/g, '%2C') + '/changecase?relateCase=yes'
+		switchMethodAndBody('PUT', '{"id": ' + window.bufferCase.id + ' }')
+		fetch(moveactivityURL, initObject)
+		.then(function(response){
+			if (response.ok) {
+				append2DIV('actionsLog', "<strong>&#10004;</strong> Activity " + window.srcCaseActivityIDs.toString() + " moved from source case "+
+				window.srcCaseProperties.id +" to destination case " + window.bufferCase.id);
+				egLogout()
+			} else {
+				append2DIV('actionsLog', response.status + ' ' + response.statusText + ' - something unexpected happened')
+				egLogout()
+			}})
+		})}
+
+function changeActivityCustomer(activityID, customerID){
+	append2DIV('actionsLog', 'Activity ID: ' + activityID + ', customer ' + customerID + ' must be changed' )
+	switchMethodAndBody('PUT', '{"activity": [{"id":"' + activityID + '","customer": {"contactPersons": {"contactPerson": [{"contactPoints": {"contactPoint":[{"id": "' + customerID +  '"}]}}]}}}]}')
+	var tempURL = baseUrl + '/system/ws/v12/interaction/activity/changecustomer'
+	fetch(tempURL, initObject)
+	}
+
+function changeCaseCustomer() {
+	return new Promise(function(resolve, reject) {
+	let chgCustomerURL = baseUrl + apiCaseCall + window.srcCaseProperties.id + '/changecustomer'
+	switchMethodAndBody('PUT', '{ "customer": { "id": ' + window.bufferCase.customer.id + ' }}')
+	if (window.srcCaseProperties.id != window.bufferCase.id && window.srcCaseProperties.customer.id == window.bufferCase.customer.id) {
+		append2DIV('actionsLog', 'both cases already have the same customer: ' + window.bufferCase.customer.id);
+		resolve('same customer')
+	} else {
+		fetch(chgCustomerURL, initObject)
+		.then(function(response) {
+			if (!response.ok){
+				append2DIV('actionsLog', '<strong>' + response.status + ' - ' + response.statusText + '</strong>. Something went wrong');
+				resolve('customer unchanged');
+			} else {
+				append2DIV('actionsLog', 'Customer for case ' + window.srcCaseProperties.id +' changed to ' + window.bufferCase.customer.id);
+				resolve('customer changed');
+				}})
+				}})
+	//return promise;
+	}
+
+
+
+
+//Invoke the first actions to run on page load
+let firstLoginOnPageLoad = egLogin()
+
+firstLoginOnPageLoad
 	.then(() => buildTableAndLogout('firstTable'))
+	.then(() => storeFirstCase())
